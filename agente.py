94afd7955d2d25b1065b0ffa87bdb78afcfe5de3
LeandroTimoteo <leandrinhots6@gmail.com>
2025-11-17 20:18:17 -0300

    Implementando função classificar_acao para testes

---
+++ agente.py
@@ -0,0 +1,10 @@
+def classificar_acao(texto: str) -> str:
+    texto = texto.lower()
+    if "relação de trabalho" in texto or "salário" in texto or "emprego" in texto:
+        return "trabalhista"
+    elif "contrato" in texto or "indenização" in texto or "dívida" in texto:
+        return "civil"
+    elif "crime" in texto or "pena" in texto or "acusado" in texto:
+        return "penal"
+    else:
+        return "desconhecido"
